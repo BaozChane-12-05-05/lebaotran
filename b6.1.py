128f93f7239189ac50f395bee68283bfa97a7ba8
lebaotran <lebaotrantxdh@gmail.com>
2024-08-22 20:24:57 +0700

    Add files via upload

---
+++ b6.1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 22 19:06:30 2024
+
+@author: lebaotran
+"""
+
+str_name=("Đại học quốc gia, khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM")
+print(str_name.replace(", ","\n").replace(", ","\n").replace(", ","\n"))
